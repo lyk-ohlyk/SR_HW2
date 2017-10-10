d1a61b3c129d8dde4ade6062c0440f1b5d3a8883
lyk-ohlyk <444896219@qq.com>
2017-10-10 12:56:40 +0800

    Add files via upload

--- Difference.cpp
+++ Difference.cpp
@@ -5,11 +5,27 @@
 #include "declaration.h"
 
 void Difference(float* inData, float* outData, int data_size) {
-	outData[0] = (inData[1] - inData[0] + 2 * inData[2] - 2 * inData[0]) / 10;
-	outData[1] = (inData[2] - inData[0] + 2 * inData[3] - 2 * inData[1]) / 10;
-	outData[data_size - 1] = (inData[data_size - 1] - inData[data_size - 2] + 2 * inData[data_size - 1] - 2 * inData[data_size - 3]) / 10;
-	outData[data_size - 2] = (inData[data_size - 1] - inData[data_size - 3] + 2 * inData[data_size - 2] - 2 * inData[data_size - 4]) / 10;
-	for (int i = 2; i < data_size - 2; i++) {
-		outData[i] = (inData[i + 1] - inData[i - 1] + 2 * inData[i + 2] - 2 * inData[i - 2]) / 10;
+	if (inData != outData) {
+		outData[0] = (inData[1] - inData[0] + 2 * inData[2] - 2 * inData[0]) / 10;
+		outData[1] = (inData[2] - inData[0] + 2 * inData[3] - 2 * inData[1]) / 10;
+		outData[data_size - 1] = (inData[data_size - 1] - inData[data_size - 2] + 2 * inData[data_size - 1] - 2 * inData[data_size - 3]) / 10;
+		outData[data_size - 2] = (inData[data_size - 1] - inData[data_size - 3] + 2 * inData[data_size - 2] - 2 * inData[data_size - 4]) / 10;
+		for (int i = 2; i < data_size - 2; i++) {
+			outData[i] = (inData[i + 1] - inData[i - 1] + 2 * inData[i + 2] - 2 * inData[i - 2]) / 10;
+		}
+	}
+	else {
+		float *tempData = new float[data_size];
+		tempData[0] = (inData[1] - inData[0] + 2 * inData[2] - 2 * inData[0]) / 10;
+		tempData[1] = (inData[2] - inData[0] + 2 * inData[3] - 2 * inData[1]) / 10;
+		tempData[data_size - 1] = (inData[data_size - 1] - inData[data_size - 2] + 2 * inData[data_size - 1] - 2 * inData[data_size - 3]) / 10;
+		tempData[data_size - 2] = (inData[data_size - 1] - inData[data_size - 3] + 2 * inData[data_size - 2] - 2 * inData[data_size - 4]) / 10;
+		for (int i = 2; i < data_size - 2; i++) {
+			tempData[i] = (inData[i + 1] - inData[i - 1] + 2 * inData[i + 2] - 2 * inData[i - 2]) / 10;
+		}
+		for (int i = 0; i < data_size; i++) {
+			outData[i] = tempData[i];
+		}
+		delete[] tempData;
 	}
 }
